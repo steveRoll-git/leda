1f217991dfa8357fe70ad1df2c4917958d567e39
Ron Tseytlin <ronts390@gmail.com>
2026-04-26 23:52:14 +0300

    Added glob pattern support to GenerateResults script

--- Scripts/GenerateResults.cs
+++ Scripts/GenerateResults.cs
@@ -1,30 +1,40 @@
 #:project ../Leda.Lang/Leda.Lang.csproj
 #:project ../Leda.Test/Leda.Test.csproj
+#:package Microsoft.Extensions.FileSystemGlobbing@10.0.7
 
 using Leda.Lang;
 using Leda.Test;
+using Microsoft.Extensions.FileSystemGlobbing;
 
 // Assumes the script's working directory is the solution's root.
-const string ledaTestPath = "Leda.Test/";
+const string projectPath = "Leda.Test/";
 
-var filename = args[0];
-var filenameWithoutExtension = Path.GetFileNameWithoutExtension(filename);
+var testFilesPath = Path.Join(projectPath, "tests");
+var resultFilesPath = Path.Join(projectPath, "results");
 
-var testCodePath = Path.Join(ledaTestPath, "tests", filename);
-var testDiagnosticsPath = Path.Join(ledaTestPath, "results", filenameWithoutExtension + ".diagnostics");
-var testEmittedPath = Path.Join(ledaTestPath, "results", filenameWithoutExtension + ".lua");
+var pattern = args[0];
 
-var testCode = File.ReadAllText(testCodePath);
-var source = new Source(filename, testCode);
-var diagnostics = new List<Diagnostic>();
-diagnostics.AddRange(source.Parse());
-diagnostics.AddRange(source.Bind());
-diagnostics.AddRange(source.Check());
+Matcher matcher = new();
+matcher.AddInclude(pattern);
 
-var diagnosticsOutput = DiagnosticPrinter.DiagnosticsOutput(diagnostics);
-File.WriteAllText(testDiagnosticsPath, diagnosticsOutput);
-Console.WriteLine("Generated results file at " + testDiagnosticsPath);
+foreach (var testCodePath in matcher.GetResultsInFullPath(testFilesPath))
+{
+    var filenameWithoutExtension = Path.GetFileNameWithoutExtension(testCodePath);
+    var testDiagnosticsPath = Path.Join(resultFilesPath, filenameWithoutExtension + ".diagnostics");
+    var testEmittedPath = Path.Join(resultFilesPath, filenameWithoutExtension + ".lua");
 
-var emittedCode = Emitter.Emit(source.Chunk);
-File.WriteAllText(testEmittedPath, emittedCode);
-Console.WriteLine("Generated emitted file at " + testEmittedPath);
+    var testCode = File.ReadAllText(testCodePath);
+    var source = new Source(filenameWithoutExtension, testCode);
+    var diagnostics = new List<Diagnostic>();
+    diagnostics.AddRange(source.Parse());
+    diagnostics.AddRange(source.Bind());
+    diagnostics.AddRange(source.Check());
+
+    var diagnosticsOutput = DiagnosticPrinter.DiagnosticsOutput(diagnostics);
+    File.WriteAllText(testDiagnosticsPath, diagnosticsOutput);
+    Console.WriteLine("Generated results file at " + testDiagnosticsPath);
+
+    var emittedCode = Emitter.Emit(source.Chunk);
+    File.WriteAllText(testEmittedPath, emittedCode);
+    Console.WriteLine("Generated emitted file at " + testEmittedPath);
+}
