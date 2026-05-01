48f889c8578f47792c812fbcf9fac7b61ef2479f
Ron Tseytlin <ronts390@gmail.com>
2026-05-02 00:43:45 +0300

    Add test for nillable narrowing

---
+++ Leda.Test/results/nillableNarrowing.lua
@@ -0,0 +1,15 @@
+local function f(x)
+  local r = x
+  if x then
+    local r1 = x + 1
+  else
+    local r1 = x
+  end
+  local r2 = x + 1
+  local r3 = x
+  if x then
+    local r1 = x + 1
+    return
+  end
+  local r4 = x
+end
