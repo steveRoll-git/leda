d434d2589dbbdb3d852c47c9f880f74401362b0e
Ron Tseytlin <ronts390@gmail.com>
2026-05-04 02:00:46 +0300

    Prevent renaming a type that already has a name

--- Leda.Lang/TypeEvaluator.cs
+++ Leda.Lang/TypeEvaluator.cs
@@ -498,7 +498,7 @@ public class TypeEvaluator(Source source)
     private Type GetTypeOfTypeAliasUncached(Symbol.TypeAlias typeAlias)
     {
         var type = GetTypeOfTypeAnnotation(typeAlias.Declaration.Type);
-        if (type is Type.Table)
+        if (type.Name == null && type is Type.Table)
         {
             type.Name = typeAlias.Declaration.Name.Value;
         }
